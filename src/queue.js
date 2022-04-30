d74cff5f77cdf53702698d323b37f6d92a3a7f1f
Vladimir <vovchikzaharov1991@gmail.com>
2022-04-30 09:03:21 +0300

    feat: completed the task queue

--- src/queue.js
+++ src/queue.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
@@ -13,24 +13,53 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
+// class ListNode {
+//   constructor(value) {
+//     this.value = value;
+//   }
+// }
 class Queue {
-
+  constructor() {
+    this.head = null;
+    this.body;
+  }
   getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.head;
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  enqueue(value) {
+    if(this.head === null){
+      // let list = new ListNode(value);
+      this.head = new ListNode(value);
+      this.body = this.head;
+    } else {
+      this.body.next = new ListNode(value);
+      this.body = this.body.next;
+    }
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    const deletedHead = this.head.value;
+    // this.head.shift();
+    if(this.head.next){
+      this.head = this.head.next;
+    }else{
+      this.head = null;
+      this.body = null;
+    }
+    return deletedHead;
   }
 }
 
 module.exports = {
   Queue
 };
+const queue = new Queue();
+queue.enqueue(5);
+queue.enqueue(6);
+queue.enqueue(7);
+console.log(queue)
+queue.dequeue(); // 5
+console.log(queue)
+queue.dequeue(); // 6
+console.log(queue)
